8ec4fad7004906f4f2b03f81235e1e9420d2a240
NicolasOlmos222 <nicolas0olmos@gmail.com>
2021-03-25 23:33:52 -0300

    Problema, nose porque me imprime dos veces lo mismo. Si pongo uno bajo otro (cosa que tenga que ingresar por ejemplo solo tres numeros, me saltea el ingreso del segundo y me ingresa un numero cualquiera

--- Ej1-Promedio/Ej1-Promedio.c
+++ Ej1-Promedio/Ej1-Promedio.c
@@ -1,1 +1,24 @@
-//hola
+/*Escriba un programa que calcule e imprima el promedio de varios enteros.
+  Debe pedir al usuario que primero ingrese los números que desea uno a uno. 
+  Cuando haya ingresado los números deseados debe ingresar la letra P, para así proceder al cálculo.*/
+#include <stdio.h>
+  
+char valor;
+int numeros = 0;
+  
+int main(){
+	printf("Ingrese los numeros que se desee, al finalizar ingrese P.\n");
+	
+	while(1){
+		printf ("Ingrese un valor: ");
+		valor = getc(stdin);
+		if(valor != 'p' && valor != 'P'){
+			numeros += (valor - '0');
+			printf("%d", numeros);
+		}else break;
+		//putc (valor, stdout);
+	}
+	printf("%d", numeros);
+	
+	return(0);
+}
