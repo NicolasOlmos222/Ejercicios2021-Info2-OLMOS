e9bae0ca05c72370671e8ff9199c58842b6f6fd8
NicolasOlmos222 <nicolas0olmos@gmail.com>
2021-04-05 21:39:58 -0300

    Signed-off-by: NicolasOlmos222 <nicolas0olmos@gmail.com>

--- Ej3-Arreglos/Ej3-Arreglos.c
+++ Ej3-Arreglos/Ej3-Arreglos.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 
 float arreglo[100];
+float arreglob[100];
 float contador = 1.0;
+float suma = 0.0;
 int i;
 
 int main(){
@@ -13,12 +15,38 @@ int main(){
 	
 	//A)
 	
-	printf("%f\n", arreglo[7]);	
+	printf("%0.2f\n", arreglo[7]);	
 	
 	//B)
 	
 	printf("Ingrese un valor para el arreglo 4: ");
 	scanf("%f", &arreglo[4]);
-
+	
+	//C)
+	
+	for(i = 0;i<=4;i++){
+		arreglo[i] = 8;
+		printf ("%0.2f\n", arreglo[i]);
+	}
+	
+	//D)
+	
+	for(i = 0;i<=99;i++){
+		suma += arreglo[i];
+	}
+	printf ("Resultado de la suma: %0.2f\n", suma);
+	
+	//E)
+	
+	printf("Arreglo a:\n");
+	for(i = 0;i<=99;i++){
+		printf("%0.2f - ", arreglo[i]);
+	}
+	printf("\nArreglo b:\n");
+	for(i = 0;i<=99;i++){
+		arreglob[i] = arreglo[i];
+		printf("%0.2f - ", arreglob[i]);
+	}
+	
 return 0;
 }
