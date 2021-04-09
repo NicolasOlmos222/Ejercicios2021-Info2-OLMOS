c85eb60a78cc13c4549022c20b1be6da2a4a8138
NicolasOlmos222 <nicolas0olmos@gmail.com>
2021-04-09 19:36:44 -0300

    Signed-off-by: NicolasOlmos222 <nicolas0olmos@gmail.com>

---
+++ Arduino/Ej5-SecuenciaLed/Ej5-SecuenciaLed.ino
@@ -0,0 +1,15 @@
+/*  Ej5-SecuenciaLed: Prender secuencialmente 3 leds. En todo momento habrá encendido solamente 1 led.
+ *  Utilizar 2 pulsadores para controlar el sentido de encendido. Por ejemplo:
+ *  Pulsador A: led1 > led2 > led3 > led1 > …
+ *  Pulsador B: led1 > led3 > led2 > led1 > … 
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
