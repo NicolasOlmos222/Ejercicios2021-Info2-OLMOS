86c9ca85d28643e08feb500770fc8a14224cf330
NicolasOlmos222 <nicolas0olmos@gmail.com>
2021-04-09 20:13:38 -0300

    Terminado
    Signed-off-by: NicolasOlmos222 <nicolas0olmos@gmail.com>

--- Arduino/Ej5-SecuenciaLed/Ej5-SecuenciaLed.ino
+++ Arduino/Ej5-SecuenciaLed/Ej5-SecuenciaLed.ino
@@ -3,13 +3,79 @@
  *  Pulsador A: led1 > led2 > led3 > led1 > …
  *  Pulsador B: led1 > led3 > led2 > led1 > … 
  */
+#include "antiRebote.h"
+
+#define PIN_LED2 5
+#define PIN_LED1 4
+#define PIN_LED0 3
+
+int cuenta = 0;
+bool cicloActivo;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  pulsador_config ();
+  pinMode(PIN_LED0, OUTPUT);
+  pinMode(PIN_LED1, OUTPUT);
+  pinMode(PIN_LED2, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  cicloActivo = pulsador_cicloA();
+  cicloActivo = pulsador_cicloB();
+  
+  switch(cicloActivo){
+    case 0:
+      cicloA();
+      break;
+    case 1:
+      cicloB();
+      break;
+  }
+  delay(500);
+}
 
+void cicloA(){
+  switch(cuenta){
+    case 0:
+      digitalWrite(PIN_LED0, HIGH);
+      digitalWrite(PIN_LED1, LOW);
+      digitalWrite(PIN_LED2, LOW);
+      cuenta++;
+    break;
+    case 1:
+      digitalWrite(PIN_LED0, LOW);
+      digitalWrite(PIN_LED1, HIGH);
+      digitalWrite(PIN_LED2, LOW);
+      cuenta++;
+    break;
+    case 3:
+      digitalWrite(PIN_LED0, LOW);
+      digitalWrite(PIN_LED1, LOW);
+      digitalWrite(PIN_LED2, HIGH);
+      cuenta = 0;
+    break;
+  }
+}
+
+void cicloB(){
+    switch(cuenta){
+    case 0:
+      digitalWrite(PIN_LED0, HIGH);
+      digitalWrite(PIN_LED1, LOW);
+      digitalWrite(PIN_LED2, LOW);
+      cuenta++;
+    break;
+    case 1:
+      digitalWrite(PIN_LED0, LOW);
+      digitalWrite(PIN_LED1, LOW);
+      digitalWrite(PIN_LED2, HIGH);
+      cuenta++;
+    break;
+    case 3:
+      digitalWrite(PIN_LED0, LOW);
+      digitalWrite(PIN_LED1, HIGH);
+      digitalWrite(PIN_LED2, LOW);
+      cuenta = 0;
+    break;
+  }
 }
---
+++ Arduino/Ej5-SecuenciaLed/antiRebote.h
@@ -0,0 +1,8 @@
+#include <arduino.h>
+
+#define PIN_PULSADOR1 6
+#define PIN_PULSADOR0 5
+
+void pulsador_config (void);
+bool pulsador_cicloA (void);
+bool pulsador_cicloB (void);
