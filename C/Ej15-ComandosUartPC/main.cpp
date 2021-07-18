f61e39f6c6a66d5de11fe7867e312e303352e432
NicolasOlmos222 <80790398+NicolasOlmos222@users.noreply.github.com>
2021-07-18 18:40:14 -0300

    Actualizo el 15

--- C/Ej15-ComandosUartPC/main.cpp
+++ C/Ej15-ComandosUartPC/main.cpp
@@ -1,58 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <Windows.h>
-#include "rs232.h"
+#include "rs-232.h"
 
-//================== Definiciones ==========================
 #define LONG_BUFFER 4096
 
-//================== Programa principal ==========================
-int main()
-{
-  //---------------- Configuraciones ------------------------
-  int puertoCOM=4;             //Número de puerto. 6 es para el COM7 en windows
-  int baudios=115200;            //Velocidad en baudios
-  char modo[]={'8','N','1',0}; // 8 bits de datos, no paridad, 1 bit de parada
+char letra;
+main(){
+	int puertoCOM = 3;             //COM DEL ARDUINO
+  	int baudios = 9600;            //Velocidad en baudios
+ 	char modo[]={'8','N','1',0}; // 8 bits de datos, no paridad, 1 bit de parada
 
-  unsigned char bufferRecepcion[LONG_BUFFER];
-  int cantidadBytes;
+  	unsigned char bufferRecepcion[LONG_BUFFER];
+  	int cantidadBytes;
 
 
-  if(RS232_OpenComport(puertoCOM, baudios, modo, 0)) //Prueba abrir el puerto, devuelve 1 en caso de error
-  {
-    printf("No se puede abrir el puerto COM\n");
-    return(0);
-  }
+  	if(RS232_OpenComport(puertoCOM, baudios, modo, 0)){
+    	printf("No se puede abrir el puerto COM\n");
+    	return(0);
+  	}
 
-  //---------------- Bucle infinito ------------------------
-  while(TRUE)
-  {
-    //Envía un byte sobre el puerto COM elegido
-    RS232_SendByte(puertoCOM, 'E');
-    Sleep(1000); //Retardo de 100 ms
+	while(TRUE){
+		printf("Ingrese un comando: ");
+		scanf("%c", &letra);
+        RS232_SendByte(puertoCOM, letra);
+    	Sleep(1000);
     
-    //Lee los datos almacenados en el buffer del puerto COM y los guarda en bufferRecepcion
-    //Devuelve la cantidad de bytes leídos
-    cantidadBytes = RS232_PollComport(puertoCOM, bufferRecepcion, LONG_BUFFER-1);
+    	cantidadBytes = RS232_PollComport(puertoCOM, bufferRecepcion, LONG_BUFFER-1);
 
-    if(cantidadBytes > 0)
-    {
-      bufferRecepcion[cantidadBytes] = 0;   // Poner un NULL al final del string
+    	if(cantidadBytes > 0){
+      		bufferRecepcion[cantidadBytes] = 0;   // Pone un NULL al final del string
+			printf("%s\n", (char *)bufferRecepcion);
+    	}
 
-      printf("Se recibieron %i bytes: %s\n", cantidadBytes, (char *)bufferRecepcion);
-    }
-
-    RS232_SendByte(puertoCOM, 'A');
-    Sleep(1000); //Retardo de 100 ms
-
-    cantidadBytes = RS232_PollComport(puertoCOM, bufferRecepcion, LONG_BUFFER-1);
-
-    if(cantidadBytes > 0)
-    {
-      bufferRecepcion[cantidadBytes] = 0;   // Poner un NULL al final del string
-
-      printf("Se recibieron %i bytes: %s\n", cantidadBytes, (char *)bufferRecepcion);
-    }
 
   }
 
